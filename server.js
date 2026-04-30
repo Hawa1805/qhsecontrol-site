7e6530a4d9cfa3dd35d0a2ce05edffeae78bdf50
Hawa1805 <hawa1805@github.com>
2026-04-30 22:38:56 +0000

    Initial commit - QHSE Control landing page + Railway config

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Servir les fichiers statiques
+app.use(express.static(path.join(__dirname)));
+
+// Toutes les routes renvoient index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`QHSE Control site running on port ${PORT}`);
+});
